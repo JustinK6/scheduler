ee1ca09dcf5d9a7f748c9ef3984b41a16ef8062f
unknown <justink6@illinois.edu>
2021-08-15 11:41:46 -0500

    studentview

---
+++ client/src/Pages/StudentView.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Button } from "react-bootstrap"
+import { useHistory } from "react-router"
+
+function TeacherView() {
+  // show beginning student view of their existing schedule
+
+  return (
+    <div>
+      <Button variant="primary">Add Class</Button>{' '}
+    </div>
+  );
+}
+
+export default TeacherView;
