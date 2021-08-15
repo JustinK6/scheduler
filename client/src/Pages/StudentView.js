510466384bc679b9d0785143a56963203befc66f
unknown <justink6@illinois.edu>
2021-08-15 11:52:16 -0500

    scheduler

--- client/src/Pages/StudentView.js
+++ client/src/Pages/StudentView.js
@@ -4,10 +4,38 @@ import { useHistory } from "react-router"
 
 function StudentView() {
   // show beginning student view of their existing schedule
+  const [classAddCode, setClassAddCode] = useState('');
+  const [classDelCode, setClassDelCode] = useState('');
 
   return (
     <div>
-      <Button variant="primary">Add Class</Button>{' '}
+        <h1 className="App">Scheduler</h1>
+        <h2 className="App">Add Or Remove Classes</h2>
+        <Col xs={6}>
+          <Alert variant='primary'>Add Class</Alert>
+          <Form>
+            <Form.Group>
+              <Form.Label>Enter Class Code</Form.Label>
+              <Form.Control placeholder="Enter Class Code" type="text" onChange={(e)=>{setClassAddCode(e.target.value)}}/>
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={addIngredient}>
+              Submit
+            </Button>
+          </Form>
+
+          <Alert variant='light' />
+
+          <Alert variant='primary'>Delete Class</Alert>
+          <Form>
+            <Form.Group>
+              <Form.Label>Enter Class Code</Form.Label>
+              <Form.Control placeholder="Enter Class Code" type="text" onChange={(e)=>{setClassDelCode(e.target.value)}}/>
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={deleteIngredient}>
+              Submit
+            </Button>
+          </Form>`
+        </Col>
     </div>
   );
 }
